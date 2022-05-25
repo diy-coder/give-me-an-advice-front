5f426f41a45ca3ebcba91f37b0d5da3efbc2edb7
DIY Coder - Someone who really likes to code <diy-coder@outlook.com>
2022-05-26 00:25:38 +0100

    Melhorando navegabilidade

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormConselhoComponent } from './componentes/forms/conselho/form-conselho/form-conselho.component';
+import { ListConselhoComponent } from './componentes/forms/conselho/list-conselho/list-conselho.component';
+import { HomeComponent } from './componentes/forms/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'conselhos',
+    component: ListConselhoComponent,
+  },
+  {
+    path: 'conselhos/:identificador',
+    component: FormConselhoComponent,
+  },
+  {
+    path: '**',
+    component: HomeComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
