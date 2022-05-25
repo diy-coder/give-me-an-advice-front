c98d3afd913dae2464d0bcf5c577484696a40def
DIY Coder - Someone who really likes to code <diy-coder@outlook.com>
2022-05-26 00:27:00 +0100

    Adicionando material componentes ao projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from './componentes/forms/forms.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
