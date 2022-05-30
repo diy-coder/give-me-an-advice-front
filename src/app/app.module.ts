4c72200815ba19e2600231652dba7584e052ab96
DIY Coder - Someone who really likes to code <diy-coder@outlook.com>
2022-05-30 09:31:09 +0100

    Adicionando sincronismo com o backend online

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from './componentes/forms/forms.module';
 import { MenuModule } from './componentes/menu/menu.module';
-
+import Amplify from 'aws-amplify';
+import awsconfig from '../aws-exports';
+import AWSAppSyncClient from 'aws-appsync';
+Amplify.configure(awsconfig);
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -20,4 +23,18 @@ import { MenuModule } from './componentes/menu/menu.module';
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    new AWSAppSyncClient({
+      url: awsconfig.aws_appsync_graphqlEndpoint,
+      region: awsconfig.aws_appsync_region,
+      auth: {
+        type: 'API_KEY',
+        apiKey: awsconfig.aws_appsync_apiKey,
+      },
+      cacheOptions: {
+        dataIdFromObject: (obj) => `${obj.__typename}:${obj.id}`,
+      },
+    });
+  }
+}
