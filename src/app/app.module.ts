dcfeeb7317c2f1e64ea5cece73d7d3d9c30c342d
DIY Coder - Someone who really likes to code <diy-coder@outlook.com>
2022-05-30 10:12:43 +0100

    Configurações extras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import Amplify from 'aws-amplify';
+import AWSAppSyncClient from 'aws-appsync';
+import awsconfig from '../aws-exports';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from './componentes/forms/forms.module';
 import { MenuModule } from './componentes/menu/menu.module';
-import Amplify from 'aws-amplify';
-import awsconfig from '../aws-exports';
-import AWSAppSyncClient from 'aws-appsync';
 Amplify.configure(awsconfig);
 @NgModule({
   declarations: [AppComponent],
