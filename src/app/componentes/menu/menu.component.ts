703010fe44a31b6c1927e7a4db22b248a167c384
DIY Coder - Someone who really likes to code <diy-coder@outlook.com>
2022-06-01 01:00:25 +0100

    refatoração de compontes

--- src/app/componentes/menu/menu.component.ts
+++ src/app/componentes/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { Auth } from 'aws-amplify';
 
 @Component({
@@ -7,11 +7,108 @@ import { Auth } from 'aws-amplify';
   styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent implements OnInit {
-  constructor() {}
+  userInfo;
+  title;
+  activeProfile;
 
-  ngOnInit(): void {}
+  constructor(private element: ElementRef) {}
+
+  ngOnInit(): void {
+    const navbar: HTMLElement = this.element.nativeElement;
+    this.toggleButton = navbar.getElementsByClassName('navbar-toggler')[0];
+  }
 
   logout() {
+    Auth.signOut();
+  }
+
+  mobile_menu_visible: any = 0;
+  private sidebarVisible: boolean = false;
+  private toggleButton: any;
+  private $layer;
+
+  sidebarOpen() {
+    const toggleButton = this.toggleButton;
+    const body = document.getElementsByTagName('body')[0];
+    setTimeout(function () {
+      toggleButton.classList.add('toggled');
+    }, 500);
+
+    body.classList.add('nav-open');
+
+    this.sidebarVisible = true;
+  }
+  sidebarClose() {
+    const body = document.getElementsByTagName('body')[0];
+    this.toggleButton.classList.remove('toggled');
+    this.sidebarVisible = false;
+    body.classList.remove('nav-open');
+  }
+
+  sidebarToggle() {
+    // const toggleButton = this.toggleButton;
+    // const body = document.getElementsByTagName('body')[0];
+    var $toggle = document.getElementsByClassName('navbar-toggler')[0];
+
+    if (this.sidebarVisible === false) {
+      this.sidebarOpen();
+    } else {
+      this.sidebarClose();
+    }
+    const body = document.getElementsByTagName('body')[0];
+
+    if (this.mobile_menu_visible == 1) {
+      // $('html').removeClass('nav-open');
+      body.classList.remove('nav-open');
+      if (this.$layer) {
+        this.$layer.remove();
+      }
+      setTimeout(function () {
+        $toggle.classList.remove('toggled');
+      }, 400);
+
+      this.mobile_menu_visible = 0;
+    } else {
+      setTimeout(function () {
+        $toggle.classList.add('toggled');
+      }, 430);
+
+      this.$layer = document.createElement('div');
+      this.$layer.setAttribute('class', 'close-layer');
+
+      if (body.querySelectorAll('.main-panel')) {
+        document
+          .getElementsByClassName('main-panel')[0]
+          .appendChild(this.$layer);
+      } else if (body.classList.contains('off-canvas-sidebar')) {
+        document
+          .getElementsByClassName('wrapper-full-page')[0]
+          .appendChild(this.$layer);
+      }
+
+      setTimeout(() => {
+        this.$layer.classList.add('visible');
+      }, 100);
+
+      this.$layer.onclick = () => {
+        //asign a function
+        body.classList.remove('nav-open');
+        this.mobile_menu_visible = 0;
+        this.$layer.classList.remove('visible');
+        setTimeout(() => {
+          this.$layer.remove();
+          $toggle.classList.remove('toggled');
+        }, 400);
+      };
+
+      body.classList.add('nav-open');
+      this.mobile_menu_visible = 1;
+    }
+  }
+
+  goto(page) {}
+
+  signOut() {
     Auth.signOut();
   }
 }
--- src/app/componentes/sidebar/sidebar.component.spec.ts
+++ src/app/componentes/sidebar/sidebar.component.spec.ts
@@ -1,35 +1,17 @@
-import { HttpClientTestingModule } from "@angular/common/http/testing";
-import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { RouterTestingModule } from "@angular/router/testing";
-import { EmitenteService } from "app/services/loading-service.ts/emitente-service";
-import { UsuarioService } from "app/services/loading-service.ts/usuario.service";
-import { SidebarComponent } from "./sidebar.component";
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SidebarComponent } from './sidebar.component';
 
-describe("SidebarComponent", () => {
+describe('SidebarComponent', () => {
   let component: SidebarComponent;
   let fixture: ComponentFixture<SidebarComponent>;
-  let mockUserService: UsuarioService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [SidebarComponent],
       imports: [HttpClientTestingModule, RouterTestingModule],
-      providers: [EmitenteService, UsuarioService],
     }).compileComponents();
-
-    mockUserService = TestBed.inject(UsuarioService);
-    spyOn(mockUserService, "getUserInfo").and.returnValue({
-      cognitoUserName: "",
-      email: "",
-      companyName: "",
-      grupos: [],
-      cnpj: "",
-      isAdmin: true,
-      idToken: "",
-      identityId: "",
-      usuarioNome: "",
-      razaoSocial: "",
-    });
   });
 
   beforeEach(() => {
@@ -38,7 +20,7 @@ describe("SidebarComponent", () => {
     fixture.detectChanges();
   });
 
-  it("should create", () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
